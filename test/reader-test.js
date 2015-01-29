83d6e312e970b26f48f09cbb99bcab4b6c4de0ea
Marius Craciunoiu <marius@craciunoiu.net>
2015-01-28 23:14:43 -0500

    Handle absolute urls.

--- test/reader-test.js
+++ test/reader-test.js
@@ -31,21 +31,21 @@ exports['reader'] = {
   static: function(test) {
     test.expect(5);
     var result = reader.readFile('test/fixtures/static.html');
-    test.equal(result.js[0], '/static/js/app.js', 'should have correct url');
-    test.equal(result.css[0], '/static/css/app.css', 'should have correct url');
-    test.equal(result.png[0], '/static/img/favicon.png', 'should have correct url');
-    test.equal(result['$build'][0].url, '/static/css/app.min.css', 'should have correct css build path.');
-    test.equal(result['$build'][1].url, '/static/js/app.min.js', 'should have correct js build path.');
+    test.equal(result.js[0], 'static/js/app.js', 'should have correct url');
+    test.equal(result.css[0], 'static/css/app.css', 'should have correct url');
+    test.equal(result.png[0], 'static/img/favicon.png', 'should have correct url');
+    test.equal(result['$build'][0].url, 'static/css/app.min.css', 'should have correct css build path.');
+    test.equal(result['$build'][1].url, 'static/js/app.min.js', 'should have correct js build path.');
     test.done();
   },
   jinja: function(test) {
     test.expect(5);
     var result = reader.readFile('test/fixtures/jinja.html');
-    test.equal(result.js[0], '/static/js/app.js', 'should have correct url');
-    test.equal(result.css[0], '/static/css/app.css', 'should have correct url');
-    test.equal(result.png[0], '/static/img/favicon.png', 'should have correct url');
-    test.equal(result['$build'][0].url, '/static/css/app.min.css', 'should have correct css build path.');
-    test.equal(result['$build'][1].url, '/static/js/app.min.js', 'should have correct js build path.');
+    test.equal(result.js[0], 'static/js/app.js', 'should have correct url');
+    test.equal(result.css[0], 'static/css/app.css', 'should have correct url');
+    test.equal(result.png[0], 'static/img/favicon.png', 'should have correct url');
+    test.equal(result['$build'][0].url, 'static/css/app.min.css', 'should have correct css build path.');
+    test.equal(result['$build'][1].url, 'static/js/app.min.js', 'should have correct js build path.');
     test.done();
   },
   gruntStatic: function (test) {
